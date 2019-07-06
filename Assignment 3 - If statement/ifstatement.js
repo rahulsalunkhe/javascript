bf9f833908f6dd0857f287503b019110f3e69ef0
Rahul Salunkhe <rahul.crack95@gmail.com>
2019-07-06 15:15:58 +0530

    Update ifstatement.js

--- Assignment 3 - If statement/ifstatement.js
+++ Assignment 3 - If statement/ifstatement.js
@@ -17,3 +17,26 @@ if(menMortality) //checks to mortality of men
 		}
 	}
 }
+
+
+console.log("\n");
+console.log("\n");
+
+console.log("Showing next program of cake being either chocolate or vanilla");
+
+
+/// Second program Is cake vanilla or chocolate
+
+var cakeType = "Chocolate" //Defines the type of cake 
+
+if(cakeType == "Chocolate" || cakeType == "Vanilla") //First level decision to check if cake is either chocolate or vanilla
+{
+	if(cakeType != "Vanilla") //Second level check to decide if cake is vanilla or not
+	{
+		console.log("Cake is a Chocolate one"); //True block states that the cake is chocolate one
+	}
+	else
+	{
+		console.log("Cake is a Vanilla one"); //False block states that the cake is vanilla one
+	}
+}
